aa366ea6f04889c5de6113e4a307f64f7551239f
Dallon Feldner <dallonfdev@gmail.com>
2014-07-20 18:14:47 -0500

    Bumped traceur version, did some stuff with sourcemaps

--- tasks/traceur.js
+++ tasks/traceur.js
@@ -34,19 +34,21 @@ function compileAll(grunt, compile, srcs, dest, options, callback) {
   options.filename = dest;
   if (srcs.length === 1) { // Special case for single files
     // Reference filenames directly so they display more nicely in devtools
-    options.filename = '/' + srcs[0];
+    options.filename = '/' + srcs[0].split(require('path').sep).join('/');
+    console.log(options);
   }
   compile(content, options, function(err, result) {
+    // console.log(result);
     var sourceMapName;
     if (err) {
       grunt.log.error(err);
       callback(false);
     } else {
-      if (options.sourceMap) {
+      if (options.sourceMaps) {
         sourceMapName = dest + '.map';
         result.js += '\n//# sourceMappingURL=' +
         path.basename(sourceMapName) + '\n';
-        grunt.file.write(sourceMapName, result.sourceMap);
+        grunt.file.write(sourceMapName, result.generatedSourceMap);
         grunt.log.debug('SourceMap written to "' + sourceMapName + '"');
       }
       grunt.file.write(dest, result.js, {encoding: 'utf8'});
@@ -61,7 +63,7 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('traceur',
     'Compile ES6 JavaScript to ES5 JavaScript', function() {
       var options = this.options({
-        sourceMap: true
+        sourceMaps: true
       });
       grunt.log.debug('using options: ' + JSON.stringify(options));
       var done = this.async();
